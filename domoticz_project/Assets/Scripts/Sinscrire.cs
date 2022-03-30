42df9bfeaa8031e7ac3d6635a59a3b27cb4e6d99
Aleyna <taylaneraleyna@gmail.com>
2022-03-30 17:25:37 +0200

    Added h1.tag

--- domoticz_project/Assets/Scripts/Sinscrire.cs
+++ domoticz_project/Assets/Scripts/Sinscrire.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script1 : MonoBehaviour
+public class Sinscrire : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class script1 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void appuie()
+    {
+        print("Success");
     }
 }
